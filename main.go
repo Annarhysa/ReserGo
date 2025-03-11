b5cc4917a1e6da1518dd24fe669c8aba31f428b3
Annarhysa <annarhysa.albert@nokia.com>
2025-03-11 16:45:16 +0530

    feat(for): Loop added

--- main.go
+++ main.go
@@ -2,34 +2,51 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	appName := "ReserGo"
 	const noOfTickets int = 100
 	var remainingTickets uint = 100
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v !\n", appName)
 	fmt.Printf("We have total %v tickets and %v tickets are available\n", noOfTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend the conference")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve your booking confirmation on %v email", firstName, lastName, userTickets, email)
+		remainingTickets = remainingTickets - userTickets
 
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve your booking confirmation on %v email\n", firstName, lastName, userTickets, email)
+		fmt.Printf("Now we have %v tickets available\n", remainingTickets)
+
+		firstNames := []string{}
+
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("All the bookings: %v\n", firstNames)
+	}
 }
